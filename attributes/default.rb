edbf61d78ebeb20299a3f947755c4516974acafd
Stoyan Stoyanov <stoyan.stoyanov@asideas.de>
2014-05-06 18:59:04 +0200

    use self-signed is now true

--- attributes/default.rb
+++ attributes/default.rb
@@ -57,7 +57,7 @@ default[:nexus][:app_server][:jetty][:loopback]                = false
 default[:nexus][:app_server_proxy][:ssl][:port]                = 8443
 default[:nexus][:app_server_proxy][:ssl][:key]                 = node[:fqdn]
 
-default[:nexus][:app_server_proxy][:use_self_signed]  = false
+default[:nexus][:app_server_proxy][:use_self_signed]  = true
 default[:nexus][:app_server_proxy][:server_name]      = node[:fqdn]
 default[:nexus][:app_server_proxy][:port]             = "http://127.0.0.1:#{node[:nexus][:port]}"
 default[:nexus][:app_server_proxy][:server][:options] = [
